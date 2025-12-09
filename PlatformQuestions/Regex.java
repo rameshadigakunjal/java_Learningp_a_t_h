0c7735de11cd1cca9c9501423365a4bd4f68776e
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-09 16:37:18 +0530

    Regex

---
+++ PlatformQuestions/Regex.java
@@ -0,0 +1,25 @@
+package PlatformQuestions;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Regex {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = Integer.parseInt(sc.nextLine());
+        while (testCase>0) {
+            String pattern = sc.nextLine();
+            try{
+                Pattern.compile(pattern);   
+                System.out.println("Valid");
+            }
+            catch(Exception e){
+                System.out.println("Invalid");
+            }
+            testCase--;
+        }
+    }
+    
+}
