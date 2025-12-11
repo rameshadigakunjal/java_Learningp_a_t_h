95859bac5eb34472ea9ac7e51c28b5bd8c374bbe
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-11 20:41:23 +0530

    fStage1

---
+++ FoundationStage/variables.java
@@ -0,0 +1,13 @@
+package FoundationStage;
+
+public class variables {
+    public static void main(String[] args) {
+        int age = 35;
+        String name = "John";
+        double height = 5.9;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+    }
+}
