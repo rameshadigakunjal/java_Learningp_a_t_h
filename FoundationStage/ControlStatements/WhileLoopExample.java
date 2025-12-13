63b0df110ef72e3aa23129c480e6ebf0a6027ad5
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-13 15:39:21 +0530

    ControlStatement2

--- FoundationStage/ControlStatements/WhileLoopExample.java
+++ FoundationStage/ControlStatements/WhileLoopExample.java
@@ -1,5 +1,11 @@
 package FoundationStage.ControlStatements;
 
 public class WhileLoopExample {
-    
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Count: " + count);
+            count++;
+        }
+    }
 }
