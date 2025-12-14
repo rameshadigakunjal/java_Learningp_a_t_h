da72420b1c95a7407582a500f378a358cb8a888b
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-14 10:21:30 +0530

    ReadFileLength

---
+++ PlatformQuestions/FileContentLength.java
@@ -0,0 +1,30 @@
+package PlatformQuestions;
+
+//import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileContentLength {
+
+    public static int getFileContentLenghth(String filePath){
+        try{
+            return Files.readAllBytes(Paths.get(filePath)).length;
+        }
+        catch (Exception e){
+            System.out.println("Error reading file: " + e.getMessage());
+            return -1;
+        }
+    }
+    public static void main(String[] args) {
+        String filePath = "D:\\javaLearning\\IntermediateStage\\note2.txt";
+        int length = getFileContentLenghth(filePath);
+
+        if (length != -1) {
+            System.out.println("File content length: " +length);
+        } else {
+            System.out.println("Failed to read file.");
+        }
+       // System.out.println(getFileContentLenghth(filePath));
+
+    }
+}
