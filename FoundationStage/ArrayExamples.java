f8530dcc9141adc46a51042c6a9e33b5c4b9de97
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-15 15:14:39 +0530

    Arrays_And_Methods

---
+++ FoundationStage/ArrayExamples.java
@@ -0,0 +1,12 @@
+package FoundationStage;
+
+public class ArrayExamples {
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 67, 123, 47};
+        int sum = 0;
+        for(int num: arr){
+            sum += num;
+        }
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
---
+++ FoundationStage/MethodsExample.java
@@ -0,0 +1,26 @@
+package FoundationStage;
+
+class AddMethods{
+    public int methods(int x,int y){
+        return x+y;
+    }
+    public String methods(String a, String b){
+        return a+b;
+    }
+    public boolean methods(boolean p, boolean q){
+        return p&&q;
+    }
+    public double methods(double m, double n){
+        return m/n;
+    }
+
+}
+public class MethodsExample {
+    public static void main(String[] args) {
+        AddMethods obj = new AddMethods();
+        System.out.println("Addition of two numbers: " + obj.methods(5,10));
+        System.out.println("Concatenation of two strings: " + obj.methods("Hello ","World"));
+        System.out.println("Logical AND of two booleans: " + obj.methods(true,false));
+        System.out.println("Division of two doubles: " + obj.methods(15.5,2.5));
+    }
+}
