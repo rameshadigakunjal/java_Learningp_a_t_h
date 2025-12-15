5006f75268b31235a96fd92e9315014063ac0916
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-15 15:20:41 +0530

    ArrayMethods

---
+++ FoundationStage/ArrayMethods.java
@@ -0,0 +1,15 @@
+package FoundationStage;
+
+public class ArrayMethods {
+    static void getArrayLength(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            // Just iterating through the array
+            System.out.println(arr[i]);
+        }
+
+    }
+    public static void main(String[] args) {
+        int[] myArray = {10, 20, 30, 40, 50};
+        getArrayLength(myArray);
+    }
+}
