a39fee73951344ac9ead1bdc0303615900b7f7f4
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-21 16:07:58 +0530

    Static keyword

---
+++ FoundationStage/StaticKeyword/StaticMethod.java
@@ -0,0 +1,13 @@
+package FoundationStage.StaticKeyword;
+
+class Calculator{
+    static int add(int a,int b){
+        return a + b;
+    }
+
+}
+public class StaticMethod {
+    public static void main(String[] args) {
+        System.out.println("Add Two Number is: "+Calculator.add(450, 60));
+    }
+}
