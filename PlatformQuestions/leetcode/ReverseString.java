331b3a8dc5926a379f5bacf6b443b37764c33fb2
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-22 17:52:17 +0530

    Reverse String

---
+++ PlatformQuestions/leetcode/ReverseString.java
@@ -0,0 +1,48 @@
+package PlatformQuestions.leetcode;
+/*344. Reverse String
+
+Topics
+premium lock icon
+Companies
+Hint
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]  */
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+
+        while(left<right){
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+
+            left++ ;
+            right--;
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseString rs = new ReverseString();
+        char[] s = {'h','e','l','l','o'};
+        rs.reverseString(s);
+        for(char c : s){
+            System.out.print(c + " ");
+        }
+    }
+    
+}
