2d3fbab9679ff6a6d9013138bc4babc46f481f7f
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-05 17:23:40 +0530

    FunctionInterfaceAnnotation

--- AdvanceStage/Annotations/FunctionInterface.java
+++ AdvanceStage/Annotations/FunctionInterface.java
@@ -1,5 +1,12 @@
 package AdvanceStage.Annotations;
-
+@FunctionalInterface
+interface Calculator {
+    int add(int a, int b);
+}
 public class FunctionInterface {
-    
+    public static void main(String[] args) {
+        Calculator calc = (a, b) -> a + b;
+        int result = calc.add(5, 10);
+        System.out.println("Sum: " + result);
+    }
 }
