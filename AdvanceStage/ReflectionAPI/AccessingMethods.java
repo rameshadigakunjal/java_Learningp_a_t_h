7dceab6931998b1dfef5e512056f8b821faaec2a
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-07 18:09:09 +0530

    Access the methods1

--- AdvanceStage/ReflectionAPI/AccessingMethods.java
+++ AdvanceStage/ReflectionAPI/AccessingMethods.java
@@ -1,5 +1,26 @@
 package AdvanceStage.ReflectionAPI;
+class cls {
+    private int id;
+    private String name;
 
+    public cls(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 public class AccessingMethods {
-    
+    public static void main(String[] args) {
+        java.lang.reflect.Method[] methods = cls.class.getDeclaredMethods();
+        for (java.lang.reflect.Method method : methods) {
+            System.out.println(method);
+        }
+    }
 }
