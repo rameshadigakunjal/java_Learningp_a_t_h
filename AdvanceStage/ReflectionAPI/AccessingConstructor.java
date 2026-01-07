fbeadb99b986ebe19ea04acecd134447d06a6926
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-01-07 18:06:42 +0530

    Access the methods

--- AdvanceStage/ReflectionAPI/AccessingConstructor.java
+++ AdvanceStage/ReflectionAPI/AccessingConstructor.java
@@ -1,5 +1,29 @@
 package AdvanceStage.ReflectionAPI;
+import java.lang.reflect.Constructor;
+class cls {
+    private int id;
+    private String name;
+
+    public cls(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
 
 public class AccessingConstructor {
-    
+    public static void main(String[] args) {
+        Constructor<?>[] constructors = cls.class.getDeclaredConstructors();
+
+        for (Constructor<?> c : constructors) {
+            System.out.println(c);
+        }
+    }
 }
